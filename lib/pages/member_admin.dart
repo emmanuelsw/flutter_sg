dc13f7efa32f3590af7744724edac52de1365152
EmmanuelSW <edhgt89@hotmail.com>
2019-05-28 00:52:39 -0500

    Tabs

--- lib/pages/member_admin.dart
+++ lib/pages/member_admin.dart
@@ -1,35 +1,56 @@
-import 'package:course_flutter/pages/home.dart';
 import 'package:flutter/material.dart';
+
+import './home.dart';
+import './member_create.dart';
+import './member_list.dart';
 
 class MemberAdmin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        child: Column(
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              AppBar(
+                title: Text('Menu'),
+              ),
+              ListTile(
+                title: Text('All members'),
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => HomePage(),
+                    ),
+                  );
+                },
+              )
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: Text('Member Admin'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                text: 'Create Member',
+                icon: Icon(Icons.create),
+              ),
+              Tab(
+                text: 'My Members',
+                icon: Icon(Icons.list),
+              )
+            ],
+          ),
+        ),
+        body: TabBarView(
           children: <Widget>[
-            AppBar(
-              title: Text('Menu'),
-            ),
-            ListTile(
-              title: Text('All members'),
-              onTap: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (BuildContext context) => HomePage(),
-                  ),
-                );
-              },
-            )
+            MemberCreatePage(),
+            MemberListPage()
           ],
         ),
-      ),
-      appBar: AppBar(
-        title: Text('Member Admin'),
-      ),
-      body: Center(
-        child: Text('Member Admin page'),
       ),
     );
   }
---
+++ lib/pages/member_list.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MemberListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('All Members'),);
+  }
+}
