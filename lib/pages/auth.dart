9f4be429701a053c2d2c35cd90cb27f6b41db9c0
EmmanuelSW <edhgt89@hotmail.com>
2019-06-01 00:27:51 -0500

    Refactoring auth page UI

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -10,91 +10,90 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   String email, password;
 
+  Widget loginLogo() {
+    return Column(
+      children: <Widget>[
+        Image.asset('assets/logo.png', scale: 7.0),
+        SizedBox(height: 6.0),
+        Text(
+          '『 Sakura List 』',
+          style: TextStyle(
+            fontSize: 22.0,
+            fontWeight: FontWeight.w600,
+            color: Colors.pink[600],
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget loginTextField(field, label) {
+    return TextField(
+      obscureText: label == 'Password' ? true : false,
+      keyboardType: TextInputType.emailAddress,
+      style: TextStyle(fontSize: 14.0),
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(15.0),
+        hasFloatingPlaceholder: false,
+        fillColor: Colors.grey[300],
+        filled: true,
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: new BorderRadius.circular(5.0),
+        ),
+        labelText: label,
+      ),
+      onChanged: (String value) {
+        setState(() {
+          field = value;
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.only(top: 80.0, right: 15.0, left: 15.0),
-        child: Column(
-          children: <Widget>[
-            Center(
-              child: Column(
-                children: <Widget>[
-                  Image.asset('assets/logo.png', scale: 7.0),
-                  SizedBox(height: 6.0),
-                  Text(
-                    '『 Sakura List 』',
-                    style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.w600, color: Colors.pink[600]),
-                  )
-                ],
-              ),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage('assets/bg.png'), fit: BoxFit.cover),
             ),
-            SizedBox(height: 40.0),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              style: TextStyle(fontSize: 14.0),
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.all(15.0),
-                hasFloatingPlaceholder: false,
-                fillColor: Colors.grey[300],
-                filled: true,
-                border: OutlineInputBorder(
-                  borderSide: BorderSide.none,
-                  borderRadius: new BorderRadius.circular(5.0),
-                ),
-                labelText: 'Email',
-              ),
-              onChanged: (String value) {
-                setState(() {
-                  email = value;
-                });
-              },
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 80.0, right: 15.0, left: 15.0),
+            child: Column(
+              children: <Widget>[
+                loginLogo(),
+                SizedBox(height: 40.0),
+                loginTextField(email, 'Email'),
+                SizedBox(height: 10.0),
+                loginTextField(password, 'Password'),
+                SizedBox(height: 8.0),
+                SizedBox(
+                  width: double.infinity,
+                  child: RawMaterialButton(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+                    padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                    fillColor: Colors.teal,
+                    textStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text('Login'),
+                        Icon(Icons.navigate_next),
+                      ],
+                    ),
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, '/members');
+                    },
+                  ),
+                )
+              ],
             ),
-            SizedBox(height: 10.0),
-            TextField(
-              obscureText: true,
-              style: TextStyle(fontSize: 14.0),
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.all(15.0),
-                hasFloatingPlaceholder: false,
-                fillColor: Colors.grey[300],
-                filled: true,
-                border: OutlineInputBorder(
-                  borderSide: BorderSide.none,
-                  borderRadius: new BorderRadius.circular(5.0),
-                ),
-                labelText: 'Password',
-              ),
-              onChanged: (String value) {
-                setState(() {
-                  password = value;
-                });
-              },
-            ),
-            SizedBox(height: 8.0),
-            SizedBox(
-              width: double.infinity,
-              child: RawMaterialButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5.0)),
-                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                fillColor: Colors.teal,
-                textStyle:
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Text('Login'),
-                    Icon(Icons.navigate_next),
-                  ],
-                ),
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, '/members');
-                },
-              ),
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
